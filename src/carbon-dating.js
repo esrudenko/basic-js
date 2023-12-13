e11a1510554d4167b52f46916b5386b4a20994a6
Elena Mankerova <esrudenko93@gmail.com>
2023-12-13 14:09:56 +0600

    feat: solved 5 taskes

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) !== 'string' ||
+    Number(sampleActivity) <= 0 ||
+    Number(sampleActivity) > MODERN_ACTIVITY ||
+    !Number(sampleActivity)
+  ) {
+    return false;
+  } else {
+    let k = Math.log(2) / HALF_LIFE_PERIOD;
+    let kt = Math.log(MODERN_ACTIVITY / Number(sampleActivity));
+    let age = Math.ceil(kt / k);
+    return age;
+  }
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let arr = [];
+  let str = n.toString();
+
+  for (let number of str) {
+    arr.push(Number(str.replace(number, '')));
+  }
+  return Math.max(...arr);
 }
 
 module.exports = {
